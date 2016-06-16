bc618225d64008acdbb6cdbf051432ada02c4abc
Paul Frater <pnfrater@gmail.com>
2016-06-16 15:01:13 +0000

    Renamed check.names function to not mask LogbooksTranslate function.

--- R/checkNames.R
+++ R/checkNames.R
@@ -1,7 +1,7 @@
 # functions to check names of translated column names
 # sometimes it is handy to go back and see what a column name was translated from
 require(dplyr)
-check.names <- function(...) {
+check.names.fj <- function(...) {
     data(checkNames)
     nameList <- as.list(sys.call())
     name <- unlist(as.character(nameList[2:length(nameList)]))
