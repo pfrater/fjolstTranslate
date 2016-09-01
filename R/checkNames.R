013b4e773fe2c4572a4a87dff814b6eee41d0668
Paul Frater <pnfrater@gmail.com>
2016-09-01 14:48:54 +0000

    Deleted old files

--- R/checkNames.R
+++
@@ -1,9 +0,0 @@
-# functions to check names of translated column names
-# sometimes it is handy to go back and see what a column name was translated from
-require(dplyr)
-check.names.fj <- function(...) {
-    data(checkNames)
-    nameList <- as.list(sys.call())
-    name <- unlist(as.character(nameList[2:length(nameList)]))
-    return(filter(checkNames, icelandic %in% name | english %in% name))
-}
